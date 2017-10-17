7435df3972e025a1f6d205a249f97b52fa2a5c55
mehmet <memojja1907@gmail.com>
2017-10-17 17:03:18 +0300

    initial commit

---
+++ src/main/java/com/springboottdd/springboottdd/SpringBootTddApplication.java
@@ -0,0 +1,12 @@
+package com.springboottdd.springboottdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTddApplication.class, args);
+	}
+}
